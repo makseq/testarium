9744c5bc8349a895512db13f25d26e9da9a0fcb5
makseq-ubnt <makseq@gmail.com>
2018-11-18 18:33:39 +0300

    20181118.183339

--- unittests/example_ok.py
+++ unittests/example_ok.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-'''
+"""
 Testarium
 Copyright (C) 2014 Maxim Tkachenko
 
@@ -15,48 +15,60 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
+"""
 
 import testarium
 import testarium.score.fafr
 import random, json
 
+
 @testarium.experiment.set_run
 def MyRun(commit):
-	c = commit.config
-	dir = commit.dir
+    c = commit.config
+    dir = commit.dir
 
-	pos = open(dir + '/pos.txt', 'w')
-	neg = open(dir + '/neg.txt', 'w')
-	for i in xrange(100): pos.write(str(random.random()) + '\n')
-	for i in xrange(100): neg.write(str(random.random()) + '\n')
-	return 0
+    pos = open(dir + '/pos.txt', 'w')
+    neg = open(dir + '/neg.txt', 'w')
+    for i in xrange(100): pos.write(str(random.random()) + '\n')
+    for i in xrange(100): neg.write(str(random.random()) + '\n')
+    return 0
+
 
 @testarium.experiment.set_score
 def MyScore(commit):
-	d = testarium.score.fafr.Score(commit.dir)
-	d['test'] = 1
-	return d
+    d = testarium.score.fafr.Score(commit.dir)
+    d['test'] = 1
+    return d
 
-#@testarium.testarium.set_print
+
+# @testarium.testarium.set_print
 def MyPrint(commit):
-	try: a = str(commit.config['a'])
-	except: a = ''
-	
-	score = str(commit.desc['score'])
-	return ['name', 'a', 'score'], [commit.name, 'file:.testarium/testarium.json', score]
-	
+    try:
+        a = str(commit.config['a'])
+    except:
+        a = ''
+
+    score = str(commit.desc['score'])
+    return ['name', 'a', 'score'], [commit.name, 'file:.testarium/testarium.json', score]
+
+
 @testarium.testarium.set_compare
 def MyCompare(self, other):
-	if self._init:
-			if self.desc['score'] > other.desc['score']: return -1;
-			elif self.desc['score'] < other.desc['score']: return 1; 
-			else: return 0
-	else: return -1
+    if self._init:
+        if self.desc['score'] > other.desc['score']:
+            return -1
+        elif self.desc['score'] < other.desc['score']:
+            return 1
+        else:
+            return 0
+    else:
+        return -1
 
-def main():	
-	testarium.testarium.best_score_is_min()
-	testarium.main()	
-	
+
+def main():
+    testarium.testarium.best_score_is_min()
+    testarium.main()
+
+
 if __name__ == '__main__':
-	main()
+    main()
--- unittests/general_test.py
+++ unittests/general_test.py
@@ -20,86 +20,102 @@ import shutil, time, sys
 import unittest
 import subprocess
 
-#--------------------------------
+# --------------------------------
 # Remove not unittest param
 full_test = False
 pos = 0
 for v in sys.argv:
-	if v == 'full':
-		full_test = True
-		break
-	pos = pos + 1
-if full_test: del sys.argv[pos]
-#--------------------------------
+    if v == 'full':
+        full_test = True
+        break
+    pos = pos + 1
+if full_test:
+    del sys.argv[pos]
+# --------------------------------
 
 setup = False
 
+
 class TestGeneral(unittest.TestCase):
-	def setUp(self):
-		global setup
-		if setup: return 
+    def setUp(self):
+        global setup
+        if setup: return
 
-		if full_test:
-			print 'Setup ...'		
-			try: subprocess.call(['easy_install.exe', 'http://testarium.makseq.com/testarium.zip'])
-			except: subprocess.call([r'c:\Python27\Scripts\easy_install.exe', 'http://testarium.makseq.com/testarium.zip'])
-		
-		print 'Remove .testarium directory ...'
-		try: shutil.rmtree('.testarium')
-		except: pass
-		
-		setup = True
+        if full_test:
+            print 'Setup ...'
+            try:
+                subprocess.call(['easy_install.exe', 'http://testarium.makseq.com/testarium.zip'])
+            except:
+                subprocess.call([r'c:\Python27\Scripts\easy_install.exe', 'http://testarium.makseq.com/testarium.zip'])
 
-	# General
-	def test_general(self):
-		# normal start
-		import example_ok
-		example_ok.main()
-		example_ok.main()
-	
-	# MyRun
-	def test_myrun(self):
-		import example_ok
-		
-		# MyRun returns None
-		reload(example_ok)
-		def MyRun(config): return None
-		example_ok.testarium.experiment.set_run(MyRun)
-		example_ok.main()
-		
-		# MyRun returns -1
-		reload(example_ok)
-		def MyRun(config): return -1
-		example_ok.testarium.experiment.set_run(MyRun)
-		example_ok.main()
-		
-		# MyRun has broken body
-		reload(example_ok)
-		def MyRun(config): THIS_IS_BAD_PLACE
-		example_ok.testarium.experiment.set_run(MyRun)
-		example_ok.main()
+        print 'Remove .testarium directory ...'
+        try:
+            shutil.rmtree('.testarium')
+        except:
+            pass
 
-	# MyScore
-	def test_myscore(self):
-		import example_ok
-		
-		# MyScore returns None
-		reload(example_ok)
-		def MyScore(dir): return None
-		example_ok.testarium.experiment.set_score(MyScore)
-		example_ok.main()
-		
-		# MyScore has broken body
-		reload(example_ok)
-		def MyScore(dir): return THIS_IS_BAD_PLACE
-		example_ok.testarium.experiment.set_score(MyScore)
-		example_ok.main()
-		
-	def test_mail(self):
-		pass
-	
-		
+        setup = True
+
+    # General
+    def test_general(self):
+        # normal start
+        import example_ok
+        example_ok.main()
+        example_ok.main()
+
+    # MyRun
+    def test_myrun(self):
+        import example_ok
+
+        # MyRun returns None
+        reload(example_ok)
+
+        def MyRun(config): return None
+
+        example_ok.testarium.experiment.set_run(MyRun)
+        example_ok.main()
+
+        # MyRun returns -1
+        reload(example_ok)
+
+        def MyRun(config): return -1
+
+        example_ok.testarium.experiment.set_run(MyRun)
+        example_ok.main()
+
+        # MyRun has broken body
+        reload(example_ok)
+
+        def MyRun(config): THIS_IS_BAD_PLACE
+
+        example_ok.testarium.experiment.set_run(MyRun)
+        example_ok.main()
+
+    # MyScore
+    def test_myscore(self):
+        import example_ok
+
+        # MyScore returns None
+        reload(example_ok)
+
+        def MyScore(dir): return None
+
+        example_ok.testarium.experiment.set_score(MyScore)
+        example_ok.main()
+
+        # MyScore has broken body
+        reload(example_ok)
+
+        def MyScore(dir): return THIS_IS_BAD_PLACE
+
+        example_ok.testarium.experiment.set_score(MyScore)
+        example_ok.main()
+
+    def test_mail(self):
+        pass
+
+
 if __name__ == '__main__':
-	#unittest.main()
-	suite = unittest.TestLoader().loadTestsFromTestCase(TestGeneral)
-	unittest.TextTestRunner(verbosity=2).run(suite)
+    # unittest.main()
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestGeneral)
+    unittest.TextTestRunner(verbosity=2).run(suite)
